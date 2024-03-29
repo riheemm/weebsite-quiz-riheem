bfe10c9497b2840ce55873acebdf3a5f24678299
riheemm <165355719+riheemm@users.noreply.github.com>
2024-03-29 23:03:38 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+let questions= [
+    {
+        numb: 1,
+        question: "Quel est le paradigme de programmation principalement utilisé dans le langage Python ?",
+        answer: "A. programmation Orienté Objet",
+        options: [
+            "A. programmation Orienté Objet",
+            "B. programmation fonctionnelle+",
+            "C. programmation impérative"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quelles est la différence entre les méthodes HTTP GET et POST ?",
+        answer: "A. GET est limité en termes de quantité de données qu'il peut envoyer,  tandis que POST peut envoyer de grandies quantités de données",
+        options: [
+            "A. GET est limité en termes de quantité de données qu'il peut envoyer,  tandis que POST peut envoyer de grandies quantités de données",
+            "B. GET est sécurisé car il cache les données dans l'URL, tandis que POST protége les données en les envoyant dans le corps de la requête",
+            "C. GET est plus rapide que POST car il ne nécessite pas de connexion persistante avec le serveur"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Le langage de programmation utilisé coté serveur pour créer des applications web dynamiques avec des fonctionnalités est PHP ?",
+        answer: "A. Oui",
+        options: [
+            "A. Oui",
+            "B. Non",
+            "C. Je ne sais pas"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quelles est la différence entre les concepts de 'compilation' et 'interprétation' dans le contexte des langages de programmation ?",
+        answer: "A. La compilation convertit le code source en langage machine avant l'exécution, tandis que l'interprétation traduit le code ligne par ligne pendant l'exécution",
+        options: [
+            "A. La compilation convertit le code source en langage machine avant l'exécution, tandis que l'interprétation traduit le code ligne par ligne pendant l'exécution",
+            "B. La compilation est utilisée pour des langages de haut niveau, tandis que l'interprétation est utilisée pour les langages de bas niveau",
+            "C. La compilation est géneralement plus rapide que l'interprétation car elle ne nécessite pas d'analyse à chaque exécution"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quelles méthodes JavaScript sont ulilisées pour  supprimer des classes CSS à un élément HTML ?",
+        answer: "C. classList.remove()",
+        options: [
+            "A. querySelector()",
+            "B. classList.add()",
+            "C. classList.remove()"
+
+        ]
+    },
+    {
+        numb: 6,
+        question: "Le format JSON est largement utilisé pour stocker et échananger des données structurées entre un serveur et un client dans les applications web ?",
+        answer: "A. Oui",
+        options: [
+            "A. Oui",
+            "B. Non",
+            "C. je ne sais pas"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qu'est-ce que le terme 'apprentissage supervisé' désigne-t-il en intelligence artificielle ?",
+        answer: "A. Un type d'apprentissage où le modèle est formé sur un ensemble de données étiquetées.",
+        options: [
+            "A. Un type d'apprentissage où le modèle est formé sur un ensemble de données étiquetées.",
+            "B. Un type d'apprentissage où le modèle est formé sur un ensemble de données non étiquetées.",
+            "C. Un type d'apprentissage où le modèle n'est pas formé du tout."
+        ]
+    },
+    {
+        numb: 8,
+        question: "Qu'est-ce que l'IA (Intelligence Artificielle) ?",
+        answer: "A. Un ensemble de techniques visant à créer des machines capables d'imiter l'intelligence humaine.",
+        options: [
+            "A. Un ensemble de techniques visant à créer des machines capables d'imiter l'intelligence humaine.",
+            "B. Un logiciel de gestion des ressources humaines.",
+            "C. Un protocole de communication utilisé dans les réseaux informatiques."
+        ]
+    },
+    {
+        numb: 9,
+        question: "Quel langage est principalement utilisé pour interagir avec les bases de données relationnelles ?",
+        answer: "C. SQL",
+        options: [
+            "A. HTML",
+            "B. CSS",
+            "C. SQL"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Quel concept garantit que toutes les opérations dans une transaction sont exécutées avec succès ou aucune d'entre elles ne l'est ?",
+        answer: "B. Atomicité",
+        options: [
+            "A. Cohérence",
+            "B. Atomicité",
+            "C. Isolation"
+        ]
+    }
+];
